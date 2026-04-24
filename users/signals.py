f177c180c8bd22c06936df034574d4d92a65a3b1
Pepijn van Heeswijk <p.vanheeswijk@student.fontys.nl>
2026-04-24 19:57:24 +0200

    changed styling, added crew form logic

--- users/signals.py
+++ users/signals.py
@@ -1,15 +1,33 @@
 from django.db.models.signals import post_save, post_delete
 from django.contrib.auth.models import User
 
-from .models import Profiles
+from .models import Profiles, CrewMembership, Crew
+
 
 def createProfile(sender, instance, created, **kwargs):
     if created:
         user = instance
-        profile = Profiles.objects.create(
-            user = user,
-            name = user.first_name,
-            email = user.email
+        Profiles.objects.create(
+            user=user,
+            name=user.first_name,
+            email=user.email,
         )
 
+
+def createCrewMembership(sender, instance, created, **kwargs):
+    crew = instance
+    profile = getattr(crew, '_pending_member_profile', None)
+    role = getattr(crew, '_pending_member_role', 'athlete')
+
+    if profile is None:
+        return
+
+    CrewMembership.objects.create(
+        profile=profile,
+        crew=crew,
+        role=role,
+    )
+
+
 post_save.connect(createProfile, sender=User)
+post_save.connect(createCrewMembership, sender=Crew)
